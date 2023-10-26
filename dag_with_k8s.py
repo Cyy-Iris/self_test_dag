5200179498d1b4e7e1aa64dccc4c089bb69a4d16
CyyIris <yiyu.chen.imt@gmail.com>
2023-10-26 15:41:41 +0200

    test

--- dag_with_k8s.py
+++ dag_with_k8s.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+import logging
 
 default_args = {
     'owner': 'yiyu',
@@ -8,10 +9,16 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
+def log_info(**kwargs):
+    log = kwargs.get('ti').log
+    log.info("Task 1 started.")
+    # Your task logic here
+    log.info("Task 1 completed.")
+
 with DAG(
     default_args=default_args,
     dag_id="dag_with_k8s",
-    start_date=datetime(2023, 10, 24),
+    start_date=datetime(2023, 10, 26),
     schedule_interval='0 3 * * Tue-Fri'
 ) as dag:
     task1 = KubernetesPodOperator(
@@ -22,5 +29,6 @@ with DAG(
     labels={"app":"k8s_operator_test"},
     task_id="k8s_Operator_demo",
     do_xcom_push=True,
+    python_callable=log_info
 )
     task1
