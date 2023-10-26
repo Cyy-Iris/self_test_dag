7cb18b99e700a4137cd425abd06a03eea77a710d
Yiyu <82180480+Cyy-Iris@users.noreply.github.com>
2023-10-26 15:02:36 +0200

    Update k8s_operator.py

--- k8s_operator.py
+++ k8s_operator.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from airflow import DAG
+from airflow.decorators import dag, task
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 
 default_args = {
