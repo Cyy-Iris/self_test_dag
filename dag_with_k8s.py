a15c02ab36f8be4f978ff2b948448c09310bc299
CyyIris <yiyu.chen.imt@gmail.com>
2023-10-26 15:59:03 +0200

    test

--- dag_with_k8s.py
+++ dag_with_k8s.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+import numpy as np
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 import logging
@@ -8,6 +9,19 @@ default_args = {
     'retries': 5,
     'retry_delay': timedelta(minutes=5)
 }
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def test_log():
+    logger.info("Task started.")
+    try:
+        print("The task is started")
+        result = np.roots([1,-3,2])
+        logger.info("Task completed successfully. Result: %s", result)
+    except Exception as e:
+        logger.error("Task failed: %s", str(e))
+        raise
 
 with DAG(
     default_args=default_args,
@@ -23,5 +37,6 @@ with DAG(
     labels={"app":"k8s_operator_test"},
     task_id="k8s_Operator_demo",
     do_xcom_push=True,
-)
+    python_callable=test_log()
+    )
     task1
