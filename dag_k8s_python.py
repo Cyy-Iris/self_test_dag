07f039ad3cf1830d05cd0ae80ac28def3da060bd
CyyIris <yiyu.chen.imt@gmail.com>
2023-10-30 12:13:22 +0100

    test k8s operator with oython

---
+++ dag_k8s_python.py
@@ -0,0 +1,33 @@
+from airflow.decorators import task, dag
+from datetime import datetime
+
+@task.kubernetes(
+    image="python:3.8-slim-buster",
+    name="k8s_test",
+    namespace="default",
+    in_cluster=False,
+    config_file="/path/to/.kube/config",
+)
+def execute_in_k8s_pod():
+    import time
+
+    print("Hello from k8s pod")
+    time.sleep(2)
+
+@task.kubernetes(image="python:3.8-slim-buster", namespace="default", in_cluster=False)
+def print_pattern():
+    n = 5
+    for i in range(0, n):
+        # inner loop to handle number of columns
+        # values changing acc. to outer loop
+        for j in range(0, i + 1):
+            # printing stars
+            print("* ", end="")
+
+        # ending line after each row
+        print("\r")
+
+execute_in_k8s_pod_instance = execute_in_k8s_pod()
+print_pattern_instance = print_pattern()
+
+execute_in_k8s_pod_instance >> print_pattern_instance
