30ec6420930013b7150196683a2aad3ab9540d21
CyyIris <yiyu.chen.imt@gmail.com>
2023-10-31 10:09:15 +0100

    change

--- dags/main.py
+++ dags/main.py
@@ -30,10 +30,11 @@ from tasks.pdf_to_md import pdf_to_md_task
 
 
 @dag(
+    dag_id="Guillaume_code_test"
     schedule=None,
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
-    tags=["main"],
+    tags=["k8b_python"],
     params={"pdf_path": Param("path to a pdf to run the DAG on.")},
 )
 def main():
