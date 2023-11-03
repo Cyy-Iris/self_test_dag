773544ff0c78631a0aba09b97693af035afb34ea
CyyIris <yiyu.chen.imt@gmail.com>
2023-11-03 16:27:32 +0100

    test langfuse

--- dags/dag_test_external_packages.py
+++ dags/dag_test_external_packages.py
@@ -4,6 +4,10 @@ import langchain
 from airflow.decorators import dag, task
 from airflow.models.param import Param
 import pendulum
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 @dag(
     dag_id="External-python-packages",
@@ -15,6 +19,31 @@ import pendulum
 def main():
     @task()
     def test_package():
-        print("Successful!")
+        from langchain.chat_models import AzureChatOpenAI
+
+        llm = AzureChatOpenAI(
+            openai_api_base=os.getenv("OPENAI_API_BASE"),
+            openai_api_version=os.getenv("OPENAI_API_VERSION"),
+            deployment_name="PDFtoCCwithOpenAI",
+            openai_api_key=os.getenv("AZURE_OPEN_AI_KEY"),
+            openai_api_type=os.getenv("OPENAI_API_TYPE"),
+        )
+
+        from langfuse.callback import CallbackHandler
+
+        handler = CallbackHandler(
+            public_key=os.getenv("ENV_PUBLIC_KEY"),
+            secret_key=os.getenv("ENV_SECRET_KEY"),
+            host=os.getenv("ENV_HOST"),
+        )
+
+        from langchain.chains import LLMChain
+        from langchain.prompts import ChatPromptTemplate
+
+        prompt = ChatPromptTemplate.from_template("tell me a joke about {topic}")
+        chain = LLMChain(llm=llm, prompt=prompt, callbacks=[handler])
+
+        joke = chain.invoke({"topic": "an elephant and a flying saucer"})
+        print(joke)
         
 main()
