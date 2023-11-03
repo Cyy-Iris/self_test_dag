fe8486937b467b5c8045cad375a25dd2d7aa9706
CyyIris <yiyu.chen.imt@gmail.com>
2023-11-03 13:42:20 +0100

    test external packages

---
+++ dags/utils/dag_test_external_packages.py
@@ -0,0 +1,24 @@
+import langchain
+from langchain.chat_models import AzureChatOpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import ChatPromptTemplate
+from langchain.chains import LLMChain
+from langchain.prompts import ChatPromptTemplate
+import llama-index
+import flake8
+from airflow.decorators import dag, task
+from airflow.models.param import Param
+
+@dag(
+    dag_id="External-python-packages",
+    schedule=None,
+    start_date=pendulum.datetime(2023, 10,31, tz="UTC"),
+    catchup=False,
+    tags=["k8s_python"],
+)
+def main():
+    @task()
+    def test_package():
+        print("Successful!")
+        
+main()
