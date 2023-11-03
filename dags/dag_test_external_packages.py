9dcb38fd958a8dcaef2d52483c325130134bedb2
CyyIris <yiyu.chen.imt@gmail.com>
2023-11-03 16:34:41 +0100

    test package

--- dags/dag_test_external_packages.py
+++ dags/dag_test_external_packages.py
@@ -18,34 +18,13 @@ load_dotenv()
 )
 def main():
     @task()
-    def test_package():
-        from langchain.chat_models import AzureChatOpenAI
+    def test_llama():
+        help(llama_index) 
+    
+    @task()
+    def test_langchain():
+        help(langchain)
 
-        llm = AzureChatOpenAI(
-            openai_api_base=os.getenv("OPENAI_API_BASE"),
-            openai_api_version=os.getenv("OPENAI_API_VERSION"),
-            deployment_name="PDFtoCCwithOpenAI",
-            openai_api_key=os.getenv("AZURE_OPEN_AI_KEY"),
-            openai_api_type=os.getenv("OPENAI_API_TYPE"),
-        )
+    test_llama() >>test_langchain()
 
-        from langfuse.callback import CallbackHandler
-
-        handler = CallbackHandler(
-            public_key=os.getenv("ENV_PUBLIC_KEY"),
-            secret_key=os.getenv("ENV_SECRET_KEY"),
-            host=os.getenv("ENV_HOST"),
-        )
-
-        from langchain.chains import LLMChain
-        from langchain.prompts import ChatPromptTemplate
-
-        prompt = ChatPromptTemplate.from_template("tell me a joke about {topic}")
-        chain = LLMChain(llm=llm, prompt=prompt, callbacks=[handler])
-
-        joke = chain.invoke({"topic": "an elephant and a flying saucer"})
-        print(joke)
-        
-    test_package()
-        
 main()
