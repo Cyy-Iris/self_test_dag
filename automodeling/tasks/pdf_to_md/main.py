72494d722e9b5cb215c912f9cfe09f08e9365965
CyyIris <yiyu.chen.imt@gmail.com>
2023-11-14 10:10:55 +0100

    am

---
+++ automodeling/tasks/pdf_to_md/main.py
@@ -0,0 +1,17 @@
+from langchain.document_loaders import PyPDFLoader
+from automodeling.tasks.pdf_to_md.utils import (
+    remove_pages,
+    merge_pages,
+    POST_PROCESSING_STEPS,
+)
+
+
+def pdf_to_md(fpath: str) -> str:
+    loader = PyPDFLoader(fpath)
+    pages = loader.load_and_split()
+    pages = remove_pages(pages)
+    md = merge_pages(pages)
+    for post_processing_step in POST_PROCESSING_STEPS:
+        md = post_processing_step(md)
+
+    return md
