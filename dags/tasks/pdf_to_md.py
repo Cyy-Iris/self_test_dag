5b344189647585f28e352c28fc1767a0a0e1d591
CyyIris <yiyu.chen.imt@gmail.com>
2023-11-14 11:36:06 +0100

    change

--- dags/tasks/pdf_to_md.py
+++ dags/tasks/pdf_to_md.py
@@ -7,8 +7,8 @@ it exposes the following functions:
 """
 import os
 
-from utils.utils import airflow_task
-from task_logic.pdf_to_md.main import pdf_to_md
+from dags.utils.utils import airflow_task
+from dags.task_logic.pdf_to_md.main import pdf_to_md
 
 
 @airflow_task(
